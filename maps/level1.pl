68c0ffcc8231db48c4f18adcb26301f1b616de82
Flysheep1223 <q3315513581@163.com>
2025-12-04 15:52:39 +0800

    调整地图结构，为 Boss 房间区域增加墙壁定义及相关注释。

--- maps/level1.pl
+++ maps/level1.pl
@@ -7,9 +7,12 @@ health_zone(46, 14, 56, 20).
 health_zone(6, 26, 16, 32).
 
 % Map segments (Walls)
+% Above spawn point
 map_segment([22, 4], [22, 6]).
 map_segment([22, 6], [38, 6]).
 map_segment([38, 6], [38, 4]).
+
+% Boss room
 map_segment([24, 14], [24, 10]).
 map_segment([24, 10], [28, 10]).
 map_segment([32, 10], [36, 10]).
@@ -17,26 +20,40 @@ map_segment([36, 10], [36, 14]).
 map_segment([36, 18], [36, 22]).
 map_segment([36, 22], [24, 22]).
 map_segment([24, 22], [24, 18]).
+
+% Bottom-right graveyard
 map_segment([44, 4], [44, 8]).
 map_segment([48, 2], [48, 8]).
 map_segment([52, 2], [52, 8]).
 map_segment([56, 2], [56, 8]).
+
+% Bottom-left room
 map_segment([16, 12], [4, 12]).
 map_segment([4, 12], [4, 4]).
 map_segment([4, 4], [16, 4]).
 map_segment([16, 4], [16, 12]).
+
+% Middle-left room
 map_segment([4, 22], [18, 22]).
 map_segment([18, 22], [18, 16]).
 map_segment([18, 16], [4, 16]).
 map_segment([4, 16], [4, 22]).
-map_segment([32, 26], [36, 26]).
-map_segment([36, 26], [36, 30]).
-map_segment([28, 26], [24, 26]).
+
+% Top-middle room
 map_segment([24, 26], [24, 34]).
+map_segment([36, 26], [36, 28]).
+map_segment([36, 32], [36, 34]).
+
+map_segment([24, 26], [36, 26]).
+
+
+% Top-right enclosed area
 map_segment([56, 34], [56, 30]).
 map_segment([56, 30], [50, 30]).
 map_segment([50, 30], [50, 26]).
 map_segment([50, 26], [60, 26]).
+
+% Around
 map_segment([0, 0], [0, 34]).
 map_segment([0, 34], [60, 34]).
 map_segment([60, 34], [60, 0]).
