f6dbde5ce23b522a64111be5e13c0566b63563e3
Flysheep1223 <q3315513581@163.com>
2025-12-05 18:02:55 +0800

    feat(ai): 将智能窃贼纳入 AI 管理系统的初始化和回合循环

--- enemies/ai_manager.pl
+++ enemies/ai_manager.pl
@@ -5,11 +5,13 @@ init_enemies :-
     init_chaser,
     init_random_walker, % No parameters needed now
     init_hidden_bee,
-    init_timid_watched.
+    init_timid_watched,
+    init_smart_thief.
 
 % Global tick for all enemies
 enemies_tick :-
     chaser_tick,
     random_walker_tick,
     hidden_bee_tick,
-    timid_watched_tick.
+    timid_watched_tick,
+    smart_thief_tick.
